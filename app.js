3493a397d094b70e6d7b59b412f1b26a04e205a9
Arsalan Khattak <khattakdev@Arsalans-Mac-mini.local>
2021-12-01 22:51:10 +0500

    feat: add database

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const activityRouter = require("./router/activity");
-
+const Sqlite = require("sqlite3");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+const db = new Sqlite.Database("database/activity.db");
+db.run("CREATE TABLE IF NOT EXISTS activity(id TEXT, name TEXT)");
+
+app.use(bodyParser.json());
 app.use("/activity", activityRouter);
 app.listen(port);
--- controller/activity.js
+++ controller/activity.js
@@ -1,9 +1,30 @@
+const Sqlite = require("sqlite3");
+const db = new Sqlite.Database("database/activity.db");
+
 exports.getActivities = (req, res) => {
-  res.send("GET Activities");
+  db.serialize(() => {
+    db.run("SELECT * FROM activity", function (err, allRows) {
+      if (err) {
+        res.send("Something went unexpected!");
+        return console.log("Something went unexpected!", err.message);
+      }
+      res.send("RECORDS");
+      console.log(allRows);
+    });
+  });
 };
 
 exports.postActivity = (req, res) => {
-  res.send("POST Activity");
+  console.log(req.body);
+  db.serialize(() => {
+    db.run("INSERT INTO activity(id,name) VALUES(?,?)", [req.body.id, req.body.name], (err) => {
+      if (err) {
+        res.send("Something went unexpected!");
+        return console.log("Something went unexpected!");
+      }
+      res.send("Record Added!");
+    });
+  });
 };
 
 exports.deleteActivity = (req, res) => {
