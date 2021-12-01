f90e093d3390af8dea27d4578a9d4187091b6d2d
Arsalan Khattak <khattakdev@Arsalans-Mac-mini.local>
2021-12-01 23:38:04 +0500

    fix: get activites call

--- controller/activity.js
+++ controller/activity.js
@@ -2,15 +2,14 @@ const db = require("../db");
 
 exports.getActivities = (req, res) => {
   db.serialize(() => {
-    db.each("SELECT * FROM activity", function (err, allRows) {
+    db.all("SELECT * FROM activity", function (err, allRows) {
       if (err) {
         res.send("Something went unexpected!");
         return console.log("Something went unexpected!", err.message);
       }
-      return res.send([allRows]);
+      return res.send(allRows);
     });
   });
-  return res.send("No Record found!");
 };
 
 exports.postActivity = (req, res) => {
